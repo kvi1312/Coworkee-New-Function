f2ba561780db3f23b0d69e2190c98774a7b041e9
blackguy2611 <77826747+blackguy2611@users.noreply.github.com>
2022-10-09 20:04:22 +0700

    update

--- client/app/view/tablet/music/Music.js
+++ client/app/view/tablet/music/Music.js
@@ -1,79 +1,90 @@
+Ext.define("App.view.tablet.music.Music", {
+  extend: "App.view.music.Music",
+  requires: ["Ext.plugin.ListPaging"],
+  controller: "tablet-musicbrowse",
 
-Ext.define('App.view.tablet.music.Music', {
-    extend: 'App.view.music.Music',
-    requires: [
-        'Ext.plugin.ListPaging',
-    ],
-    controller: 'tablet-musicbrowse',
-
-    tbar: {
-        xtype: 'musicbrowsetoolbar'
-    },
-    items: [
+  tbar: {
+    xtype: "musicbrowsetoolbar",
+  },
+  items: [
+    {
+      xtype: "grid",
+      bind: "{music}",
+      storeId: "music",
+      columns: [
         {
-            xtype: 'grid',
-            bind: '{music}',
-            storeId:'music',
-            columns: [
-                {text: 'Name', dataIndex: 'Name', flex: 1, cell: {encodeHtml: false} , tpl: '<a class="{Id}" href="localhost:5001/music/detail/{Id}">{Name}</a>'},
-                {text: 'Author', dataIndex: 'Author', flex: 1},
-                {text: 'Link', dataIndex: 'Link', flex: 1},
-                {text: 'Active', dataIndex: 'Active', flex: 1},
-                {text: 'Description', dataIndex: 'Description', flex: 1},
-                // {text: 'Edit', id:'edit', minWidth:100, flex: 0.5, cell: {encodeHtml: false}, tpl: '<a href="localhost:5001/music/detail/{Id}><i class= "fa fa-pen"></i></a>'},
-                // {text: 'Remove',minWidth:100, flex: 0.5, cell: {encodeHtml: false}, tpl: '<a><i class= "far fa-remove"></i></a>'},
-                // {
-                    
-                //     text: "EDIT",
-                //     align: "center",
-                //     stopSelection: true,
-                //     // xtype: 'widget',
-                //     widget: {
-                //         xtype: 'button',
-                //         text: "Edit",
-                //         bind: null,
-                //         handler: function(widgetcolumn){
-                //             var record = widgetcolumn.getWidgetRecord();
-                //             console.log("got data!", record);
-                //         },
-                //         listeners: {
-                //             beforerender: function(widgetColumn){
-                //                 var record = widgetColumn.getWidgetRecord();
-                //                 widgetColumn.setText( widgetColumn._btnText ); //can be mixed with the row data if needed
-                //             }
-                //         }
-                //     }
-                // }
-                { 
-                  
-                    text: "ACTION",
+          text: "Name",
+          dataIndex: "Name",
+          flex: 1,
+          cell: { encodeHtml: false },
+          tpl: '<a class="{Id}" href="localhost:5001/music/detail/{Id}">{Name}</a>',
+        },
+        { text: "Author", dataIndex: "Author", flex: 1 },
+        { text: "Link", dataIndex: "Link", flex: 1 },
+        { text: "Active", dataIndex: "Active", flex: 1 },
+        { text: "Description", dataIndex: "Description", flex: 1 },
+        // {text: 'Edit', id:'edit', minWidth:100, flex: 0.5, cell: {encodeHtml: false}, tpl: '<a href="localhost:5001/music/detail/{Id}><i class= "fa fa-pen"></i></a>'},
+        // {text: 'Remove',minWidth:100, flex: 0.5, cell: {encodeHtml: false}, tpl: '<a><i class= "far fa-remove"></i></a>'},
+        // {
 
-                    items: [
-                    {
-                        text: 'Edit',
-                        flex: 2,
-                        
-                        handler: function(grid, rowIndex, colIndex) {
-                            var rec = grid.getStore().getAt(rowIndex);
-                            alert("Edit " + rec.get('Name'));
-                         return {tpl: '<a href="#"><i class= "fa fa-pen">aa</i></a>'}
-                        }
-                    },
-                    {
-                        
-                        icon: 'x-fa fa-cog',
-                        tooltip: 'Delete',
-                        handler: function(grid, rowIndex, colIndex) {
-                            var rec = grid.getStore().getAt(rowIndex);
-                            alert("Terminate " + rec.get('firstname'));
-                        }
-                    }]
-                }
-            ]
-        }
-    ]
-})
-
+        //     text: "EDIT",
+        //     align: "center",
+        //     stopSelection: true,
+        //     // xtype: 'widget',
+        //     widget: {
+        //         xtype: 'button',
+        //         text: "Edit",
+        //         bind: null,
+        //         handler: function(widgetcolumn){
+        //             var record = widgetcolumn.getWidgetRecord();
+        //             console.log("got data!", record);
+        //         },
+        //         listeners: {
+        //             beforerender: function(widgetColumn){
+        //                 var record = widgetColumn.getWidgetRecord();
+        //                 widgetColumn.setText( widgetColumn._btnText ); //can be mixed with the row data if needed
+        //             }
+        //         }
+        //     }
+        // }
+        {
+          text: "ACTION",
+          align: "center",
+          hideable: false,
+          id: "action-column-music",
+          flex: 0.5,
+          items: [
+            {
+              align: "right",
+              flex: 2,
+              id: "edit_music",
+              // handler: function(grid, rowIndex, colIndex) {
+              //     var rec = grid.getStore().getAt(rowIndex);
+              //     alert("Edit " + rec.get('Name'));
+              //  return {tpl: '<a href="#"><i class= "fa fa-pen">aa</i></a>'}
+              // }
+              renderer: function (params) {
+                icon = "far fa-edit";
+                id = "EditAction";
+                return `<a class="class-edit" id="${id}"><i class="${icon}"></i></a>`;
+              },
+            },
+            {
+              ialign: "left",
+              flex: 2,
+              id: "delete_music",
+              renderer: function (params) {
+                icon = "far fa-trash";
+                id = "DeleteAction";
+                return `<a class="class-delete" id="${id}"><i class="${icon}"></i></a>`;
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+});
 
 //                 {
 //                     id: 'edit_music',
@@ -84,7 +95,7 @@ Ext.define('App.view.tablet.music.Music', {
 //                         encodeHtml: false
 //                     },
 //                     tpl: [
-//                         '<button class="x-icon-el x-font-icon far fa-pen"></button>',         
+//                         '<button class="x-icon-el x-font-icon far fa-pen"></button>',
 //                     ],
 //                     // listeners: {
 //                     //     boxready: 'musicbrowse',
@@ -101,7 +112,7 @@ Ext.define('App.view.tablet.music.Music', {
 //                         encodeHtml: false
 //                     },
 //                     tpl: [
-//                         '<button class="far fa-trash"></button>',         
+//                         '<button class="far fa-trash"></button>',
 //                     ],
 //                     listener: {
 //                         click: function(){
@@ -120,7 +131,7 @@ Ext.define('App.view.tablet.music.Music', {
 //     cell: {
 //         encodeHtml: false,
 //     },
-//     columns: [ 
+//     columns: [
 //         {
 //             id: 'edit_music',
 //             align: "right",
@@ -128,8 +139,8 @@ Ext.define('App.view.tablet.music.Music', {
 //             flex: 1,
 //             cell: {
 //             encodeHtml: false,
-//             tpl: '<button class="x-icon-el x-font-icon far fa-pen"></button>'  
+//             tpl: '<button class="x-icon-el x-font-icon far fa-pen"></button>'
 //         },
-     
+
 //     ]
 // },
